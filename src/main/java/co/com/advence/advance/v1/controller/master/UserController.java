24e016c07110cae4f91534c3926f4f91b86c9940
Jesid Andrey Durango Isaza <jesid.X.durango.-ND@disney.com>
2019-09-03 10:36:43 -0500

    unused code removed

--- src/main/java/co/com/advence/advance/v1/controller/master/UserController.java
+++ src/main/java/co/com/advence/advance/v1/controller/master/UserController.java
@@ -4,11 +4,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,16 +16,10 @@ import co.com.advence.advance.v1.service.UserServiceImpl;
 
 @RestController
 @RequestMapping(path="/v1")
-public class UserController implements ApplicationContextAware {
-
-	private ApplicationContext applicationContext;
+public class UserController {
 
 	@Autowired
 	private UserServiceImpl userBisiness;
-	
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-	}
 	
 	@PostMapping("/user/sign-up")
     public void signUp(@RequestBody User user) {
