c7d36ef8a046321865e133fee0eebfafee61ed51
Jesid Andrey Durango Isaza <jesid.X.durango.-ND@disney.com>
2019-12-11 16:25:59 -0500

    last commit

--- src/main/java/co/com/advence/advance/v1/service/security/WebSecurity.java
+++ src/main/java/co/com/advence/advance/v1/service/security/WebSecurity.java
@@ -36,6 +36,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
                 // this disables session creation on Spring Security
+                
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
